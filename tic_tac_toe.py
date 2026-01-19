ce91d7c9ed49e018ef772bc882d02b581a318819
vassilakisdim <vassilakis.jim@gmail.com>
2026-01-19 22:23:46 +0200

    Updated the source code to the latest version

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -14,7 +14,8 @@ board = [" "] * 9
 ch = "default"
 zzz = ""
 
-#DEFINITIONS
+
+
 
 #GUI--------------------------------------#
 
@@ -22,18 +23,32 @@ zzz = ""
 #board[0] = "X"  #Keep disabled
 ######END_OF_DEBUG_LINE#######
 base = Tk()
-base.geometry("300x200")
+base.geometry("300x300")
 a = Label(base, text="Tic Tac Toe Game")
+g = Label(base, text="Game Ongoing")
+cfont = tk.font.Font(family='Helvetica', size=30, weight='bold')
 a.pack()
 
 
 v = tk.StringVar()
 
-# def setText(word):
-#     v.set(word)
+def end_result():
+    T = Text(base)
+end_result()
 
-# def clear_text():
-#    sel.delete(0, END)
+def reset():
+    board[0]=" "
+    board[1]=" "
+    board[2]=" "
+    board[3]=" "
+    board[4]=" "
+    board[5]=" "
+    board[6]=" "
+    board[7]=" "
+    board[8]=" "
+    g.config(text='Game Ongoing')
+    g.pack()
+    elements()
 
 
 def btn_click(i):
@@ -41,25 +56,22 @@ def btn_click(i):
     ch = i
     player_move()
     if check_winner(board, "X"):
-        print("Player won")
-        a = Label(base, text="Player Won")
+        g.config(text='Player won')
+        g.pack()
     if check_draw(board):
-        print("It's a draw")
-        a = Label(base, text="It's a draw")
-    print(" ")
+        g.config(text="It's a draw")
+        g.pack()
     ai_move()
     a_Board = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]]
     Tk.update(base)
     Tk.update_idletasks(base)
     elements()
-    #base.mainloop()
     if check_draw(board):
-        print("It's a draw")
-        a = Label(base, text="It's a draw")
-    #os.system('cls')
+        g.config(text="It's a draw")
+        g.pack()
     if check_winner(board, 0):
-        print("AI won")
-        a = Label(base, text="AI won")
+        g.config(text='AI won')
+        g.pack()
 
 # Layout for Board
 btns_frame = Frame(base)
@@ -70,60 +82,33 @@ def elements():
         for col in range(len(a_Board[row])):
             i = a_Board[row][col]
             param = str(row * 3 + col)
-            b = Button(btns_frame, text=str(i), command= partial(btn_click, param))
+            b = Button(btns_frame, text=str(i), height=1, width=2, font=cfont, command= partial(btn_click, param))
             b.pack
             b.grid(row=row + 1, column=col)
 elements()
+g.pack()
 
-# sel = (ttk.Entry(base, textvariable=""))
-# sel.pack()
-# opt = str(sel)
+res = Button(base, text="Reset", command= reset)
+res.pack()
+#End_of_GUI
 
+#DEFINITIONS
 
-
-
-# def submit():
-#     global ch
-#     global zzz
-#     ch=sel.get()
-#     print("Player Chose : " + ch)
-#     print(type(ch))
-#     zzz = True
-#     sel.set("")
-#     v.set("")
-#     clear_text()
-#     #player_move()
-
-# def submitandclear():
-#     submit()
-#     clear_text()
-    
-# b = ttk.Button(base, text="Submit", command=submitandclear)
-# b.pack()
-# #End_of_GUI
-
-
-def show(b):
-    print(f"{b[0]}|{b[1]}|{b[2]}")
-    print("-+-+-")
-    print(f"{b[3]}|{b[4]}|{b[5]}")
-    print("-+-+-")
-    print(f"{b[6]}|{b[7]}|{b[8]}")
 
 def player_move():
     while True:
         if check_draw(board):
-            print("It's a draw")
-            a = Label(base, text="It's a draw")
+            g.config(text="It's a draw")
+            g.pack()
             return
         elif check_winner(board, "X"):
-            print("Player won")
-            a = Label(base, text="Player Won")
+            g.config(text='Player Won')
+            g.pack()
             elements()
             return
         elif check_winner(board, 0):
-            print("AI won")
-            a = Label(base, text="AI won")
+            g.config(text='AI won')
+            g.pack()
             elements()
             return
         else:
@@ -136,38 +121,35 @@ def player_move():
                     zzz = True
                     break
                 else:
-                    print("taken")
+                    pass
             else:
-                print ("This is not a valid option, try again")
+                pass
         a_Board = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]]
         Tk.update(base)
     Tk.update_idletasks(base)
     elements()
-    #sel.set("")
-    show(board)
 
 def ai_move():
     free = [i for i , x in enumerate(board) if x==" "]
     #try to win
     if check_draw(board):
-        print("It's a draw")
-        a = Label(base, text="It's a draw")
+        g.config(text="It's a draw")
+        g.pack()
         elements()
         return
     if check_winner(board, "X"):
-        print("Player won")
-        a = Label(base, text="Player Won")
+        g.config(text='Player Won')
+        g.pack()
         elements()
         return
     elif check_winner(board, 0):
-        print("AI won")
-        a = Label(base, text="AI won")
+        g.config(text='AI won')
+        g.pack()
         elements()
         return
     for i in free:
         board[i]=0
         if check_winner(board, 0):
-            show(board)
             return
         else:
             board[i] = " "
@@ -176,53 +158,58 @@ def ai_move():
         board[y] = "X"
         if check_winner(board, "X"):
             board[y] = 0
-            show(board)
             return
         board[y] = " "
-        #pass
     #desperate_block_attempt
     for i in free:
         if 4 in free:
             board[4] = 0
-            show(board)
             return
         elif y in free:
             pick = random.choice(prefs)
             if board[pick] == " ":
                 board[pick] = 0
-                show(board)
                 return
         else:
             pass
     move = random.choice(free)
     board[move] = 0
-    show(board)
     a_Board = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]]
     Tk.update(base)
     Tk.update_idletasks(base)
     elements()
 
 
+def check_row(b,p):
+    row_start = [0,3,6]
+    for i in range(3):
+        if b[row_start[0]] == b[row_start[0]+1] == b[row_start[0]+2] == p:
+            return True
+        else:
+            row_start.pop(0)
 
+def check_col(b,p):
+    row_start = [0,1,2]
+    for i in range(3):
+        if b[row_start[0]] == b[row_start[0]+3] == b[row_start[0]+6] == p:
+            return True
+        else:
+            row_start.pop(0)
 
-def check_winner(b,p):
-    if b[0] == b[1] == b[2] ==  p:
-        return True
-    if b[3] == b[4] == b[5] ==  p:
-        return True
-    if b[6] == b[7] == b[8] ==  p:
-        return True
-    if b[0] == b[3] == b[6] ==  p:
-        return True
-    if b[1] == b[4] == b[7] ==  p:
-        return True
-    if b[2] == b[5] == b[8] ==  p:
-        return True
+def check_diagon(b,p):
     if b[0] == b[4] == b[8] ==  p:
         return True
     if b[2] == b[4] == b[6] ==  p:
         return True
-#if len([i for i, x in enumerate(b) if x == " "]) == 0:
+
+def check_winner(b,p):
+    if check_row(b,p):
+        return True
+    if check_col(b,p):
+        return True
+    if check_diagon(b,p):
+        return True
+    
 def check_draw(b):
     if [i for i, x in enumerate(b) if x == " "] == []:
         return True
@@ -232,43 +219,31 @@ def check_draw(b):
 #END_OF_DEFINITIONS
 
 
-show(board)
-# for i in range (2):
+turn = 0
+
+playerData = {
+    0: {"symbol": 0, "name": "AI"},
+    1: {"symbol": "X", "name": "player"},
+
+}
+
 while True:
     Tk.update(base)
     if zzz == True:
-        print(zzz)
-        player_move()
-        if check_winner(board, "X"):
-            print("Player won")
-            a = Label(base, text="Player Won")
+        if (turn % 2) == 1:
+            player_move()
+        else:
+            ai_move()
+        if check_winner(board, playerData[turn %2]["symbol"]):
+            g.config(text=(playerData[turn %2]["name"], " won"))
+            g.pack()
             break
         if check_draw(board):
-            print("It's a draw")
-            a = Label(base, text="It's a draw")
+            g.config(text="It's a draw")
+            g.pack()
             break
-        print(" ")
-        ai_move()
         a_Board = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]]]
         Tk.update(base)
         Tk.update_idletasks(base)
         elements()
-        #base.mainloop()
-        if check_draw(board):
-            print("It's a draw")
-            a = Label(base, text="It's a draw")
-            break
-        #os.system('cls')
-        if check_winner(board, 0):
-            print("AI won")
-            a = Label(base, text="AI won")
-            break
         zzz = ""
-    
-
-
-
-base.mainloop()
-
-
-
